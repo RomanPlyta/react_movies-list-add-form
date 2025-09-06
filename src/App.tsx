a85f6ad0a9d8d6995999f2724eeaccb00f7d728a
Admin <romanpluta42@gmail.com>
2025-09-06 18:56:33 +0300

    V1

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,30 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import React, { useState } from 'react';
 
-export const App = () => {
+type Movie = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
+
+export const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>(moviesFromServer);
+
+  const handleAddMovie = (movie: Movie) => {
+    setMovies(prev => [...prev, movie]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={handleAddMovie} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,98 @@
 import { useState } from 'react';
 import { TextField } from '../TextField';
+import React from 'react';
 
-export const NewMovie = () => {
+type Movie = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+};
+
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [movie, setMovie] = useState({
+    title: '',
+    description: '',
+    imgUrl: '',
+    imdbUrl: '',
+    imdbId: '',
+  });
+
+  const handleChange = (field: string, newValue: string) => {
+    setMovie(prev => ({ ...prev, [field]: newValue }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    onAdd(movie);
+
+    setMovie({
+      title: '',
+      description: '',
+      imgUrl: '',
+      imdbUrl: '',
+      imdbId: '',
+    });
+  };
+
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
   const [count] = useState(0);
 
+  const isFormValid =
+    movie.title.trim() !== '' &&
+    movie.imgUrl.trim() !== '' &&
+    movie.imdbUrl.trim() !== '' &&
+    movie.imdbId.trim() !== '';
+
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={count} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={movie.title}
+        onChange={newValue => handleChange('title', newValue)}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={movie.description}
+        onChange={newValue => handleChange('description', newValue)}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={movie.imgUrl}
+        onChange={newValue => handleChange('imgUrl', newValue)}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={movie.imdbUrl}
+        onChange={newValue => handleChange('imdbUrl', newValue)}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={movie.imdbId}
+        onChange={newValue => handleChange('imdbId', newValue)}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +100,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={!isFormValid}
           >
             Add
           </button>
